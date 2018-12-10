08125587b1e8bc73c399171dd8315d2de56ff41c
Thanh Tran <lethanhtdt@gmail.com>
2018-12-10 09:36:14 +0700

    update content

--- api/config/recipient.php
+++ api/config/recipient.php
@@ -2,10 +2,10 @@
 
 return [
     'group_1' => [
-        'contact@3forcom.com', 'binhnt@3forcom.com', 'nhutbm@3forcom.com'
+        'wook@brickmate.kr', 'binhnt@3forcom.com', 'nhutbm@3forcom.com'
     ],
     'group_2' => [
-        'contact@3forcom.com', 'binhnt@3forcom.com', 'nhutbm@3forcom.com', 'yenpham@3forcom.com'
+        'wook@brickmate.kr', 'binhnt@3forcom.com', 'nhutbm@3forcom.com', 'yenpham@3forcom.com'
     ],
     'group_test' => [
         'quanvo@3forcom.com'
